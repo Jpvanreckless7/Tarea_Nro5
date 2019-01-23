0f902b2aa6cd896386ad6774b324612347c2f2bd
Jpvanreckless <juan.lorcag@utem.cl>
2019-01-23 00:22:56 -0300

    En proceso de corrección

--- script.R
+++ script.R
@@ -21,6 +21,7 @@ webpage <- read_html(archivo)
 
 # Extrayendo contenido en la clase justificado
 contenidoWebNoticia <- html_nodes(webpage,'.justificado')
+print(contenidoWebNoticia)
 
 # Pasando la info a texto
 textoNoticia <- html_text(contenidoWebNoticia)
@@ -35,7 +36,7 @@ textoNoticia <- gsub("\"","",textoNoticia)
 textoNoticia <- gsub("[.]","",textoNoticia)
 textoNoticia <- gsub(",","",textoNoticia)
 
-# Viendo a priori la info en la variable textoNoticia
+# Viendo ordenada la info en la variable textoNoticia
 print(textoNoticia)
 
 # Separando las palabras por espacio
@@ -64,16 +65,86 @@ write.table(dfPalabrasNoticia, file="PalabrasNoticia.txt")
 ############ Extraccion información tabla ################
 ##########################################################
 
-# Extrayendo los elementos que contienen las tablas
-tablaProductos <- html_nodes(webpage, ".artefactos")
+#leyendo tabla
+Tabla <- html_nodes(webpage,'table')
+print(Tabla)
 
-# Extraccion de el contenido de las tablas usando el tag table
-contenedorDeTablas <- html_nodes(tablaProductos, "table")
+#pasando la info a tabla
+TablaWeb<-html_table(Tabla)[[1]]
 
-# Extracción información tabla 
-tabla1 <- html_table(contenedorDeTablas)
+print(TablaWeb)
 
-# Viendo el contenido de la tabla1
-print(tabla1[1])
+#Pasando informacion a un data frame
+DataFrameTabla<-as.data.frame(TablaWeb)
 
+#Almacenando la informacion en CSV
+write.csv(DataFrameTabla, file = "TablaNoticia.csv")
+
+#Eliminando los \n, comillas("), puntos(.) y comas(,) del texto
+DataFrameTabla<-gsub("\n","",DataFrameTabla)
+DataFrameTabla<-gsub("\"","",DataFrameTabla)
+DataFrameTabla<-gsub("[.]","",DataFrameTabla)
+DataFrameTabla<-gsub(",","",DataFrameTabla)
+DataFrameTabla<-gsub(":","",DataFrameTabla)
+
+#tabla despues de impiar elementos
+print(DataFrameTabla)
+
+#Sepraando las palabras por espacio
+EspacioPalabras<-strsplit(DataFrameTabla," ")[[1]]
+
+#Todas las palabras a minusculas
+MinusculaPalabras<-tolower(EspacioPalabras)
+
+#Contar palabras
+ContarPalabras<-unlist(MinusculaPalabras)
+TablaPalabras<-table(ContarPalabras)
+
+#Pasando la informacion a un data frame
+DataFramePalabras<-as.data.frame(TablaPalabras)
+
+#Juntamos los parrafos a TextoCompleto
+TextoCompleto<-""
+for(i in 1:length(DataFrameTabla))
+  TextoCompleto<-paste(TextoCompleto," ",DataFrameTabla[i])
+
+#Volvemos a realizar los pasos para contar (data frame)
+
+TextoCompleto<-gsub("\n","",TextoCompleto)
+TextoCompleto<-gsub("\"","",TextoCompleto)
+TextoCompleto<-gsub("[.]","",TextoCompleto)
+TextoCompleto<-gsub(",","",TextoCompleto)
+TextoCompleto<-gsub(":","",TextoCompleto)
+
+print(TextoCompleto)
+
+EspacioPalabras<-strsplit(TextoCompleto," ")[[1]]
+MinusculaPalabras<-tolower(EspacioPalabras)
+ContarPalabras<-unlist(MinusculaPalabras)
+TablaPalabras<-table(ContarPalabras)
+DataFramePalabras<-as.data.frame(TablaPalabras)
+write.csv(DataFramePalabras, file = "PalabrasNoticia.csv")
+
+
+#Grafico con los datos
+
+#Limpiar tabla
+TablaWeb$`Precios` <- gsub("\\$","",TablaWeb$`Precios`)
+TablaWeb$`Precios` <- gsub("[.]","",TablaWeb$`Precios`)
+TablaWeb$`Precios` <- as.numeric(gsub(",",".",TablaWeb$`Precios`))
+
+#Graficando los precios de acciones
+library('ggplot2')
+
+# respecto al precio
+TablaWeb %>%
+  ggplot() +
+  aes(x = Artefacto, y = `Precios`) +
+  geom_bar(stat="identity")
+
+# Gráfico boxplot diferenciado por producto
+TablaWeb %>%
+  ggplot() +
+  geom_boxplot(aes(x = Artefacto, y = `Precios``)) +
+  theme_bw()
 
